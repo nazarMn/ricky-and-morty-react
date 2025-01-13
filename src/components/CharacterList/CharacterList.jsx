0e871bd6374970a21cdfab6513e3e63101299a17
MarkivNazar <nazarmn2008@gmail.com>
2025-01-13 18:55:37 +0200

    added pagination

--- src/components/CharacterList/CharacterList.jsx
+++ src/components/CharacterList/CharacterList.jsx
@@ -8,19 +8,26 @@ Modal.setAppElement('#root');
 
 export default function CharacterList() {
   const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
   const [characterInfo, setCharacterInfo] = useState({});
 
   useEffect(() => {
+    fetchCharacters(page);
+  }, [page]);
+
+  const fetchCharacters = (page) => {
     axios
-      .get('https://rickandmortyapi.com/api/character')
+      .get(`https://rickandmortyapi.com/api/character?page=${page}`)
       .then((response) => {
         setCharacters(response.data.results);
+        setTotalPages(response.data.info.pages);
       })
       .catch((error) => {
         console.error('Error fetching characters:', error);
       });
-  }, []);
+  };
 
   const getCharacterInfo = (id) => {
     axios
@@ -34,25 +41,45 @@ export default function CharacterList() {
       });
   };
 
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setPage(newPage);
+    }
+  };
+
   return (
-    <div className="character-list">
-      {characters.map((character) => (
-        <Character
-          key={character.id}
-          id={character.id}
-          name={character.name}
-          species={character.species}
-          img={character.image}
-          onClick={() => getCharacterInfo(character.id)}
-        />
-      ))}
+    <div className="character-list-container">
+      <div className="character-list">
+        {characters.map((character) => (
+          <Character
+            key={character.id}
+            id={character.id}
+            name={character.name}
+            species={character.species}
+            img={character.image}
+            onClick={() => getCharacterInfo(character.id)}
+          />
+        ))}
+      </div>
+
+      <div className="pagination">
+        <button onClick={() => handlePageChange(page - 1)} disabled={page === 1}>
+          Previous
+        </button>
+        <span>
+          Page {page} of {totalPages}
+        </span>
+        <button onClick={() => handlePageChange(page + 1)} disabled={page === totalPages}>
+          Next
+        </button>
+      </div>
 
       <Modal
         isOpen={isOpen}
         onRequestClose={() => setIsOpen(false)}
         style={{
-          overlay: { backgroundColor: 'rgba(0, 0, 0, 0.75)' },
-          content: { inset: '10%', padding: 0, display: 'flex', flexDirection: 'row' },
+          overlay: { backgroundColor: 'rgba(0, 0, 0, 0.75)'},
+          content: { inset: '10%', padding: 0, display: 'flex', flexDirection: 'row', borderRadius: '18px' },
         }}
       >
         <div className="modal-content">
