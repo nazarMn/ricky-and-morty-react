3d36df50d06acd43df31dfce6064230f80b7395a
MarkivNazar <nazarmn2008@gmail.com>
2025-01-21 22:50:56 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
 import CharacterList from './components/CharacterList/CharacterList';
 import Episodes from './components/Episodes/Episodes';
 import Locations from './components/Locations/Locations';
@@ -11,27 +12,37 @@ export default function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Characters</Link>
-            </li>
-            <li>
-              <Link to="/episodes">Episodes</Link>
-            </li>
-            <li>
-              <Link to="/locations">Locations</Link>
-            </li>
-            <li>
-              <Link to="/watchlist">My Watch List</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
-          <Route path="/" element={<CharacterList />} />
-          <Route path="/episodes" element={<Episodes />} />
-          <Route path="/locations" element={<Locations />} />
-          <Route path="/watchlist" element={<WatchList />} />
+          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/*"
+            element={
+              <>
+                <nav>
+                  <ul className="nav-links">
+                    <li>
+                      <Link to="/characters">Characters</Link>
+                    </li>
+                    <li>
+                      <Link to="/episodes">Episodes</Link>
+                    </li>
+                    <li>
+                      <Link to="/locations">Locations</Link>
+                    </li>
+                    <li>
+                      <Link to="/watchlist">My Watch List</Link>
+                    </li>
+                  </ul>
+                </nav>
+                <Routes>
+                  <Route path="/characters" element={<CharacterList />} />
+                  <Route path="/episodes" element={<Episodes />} />
+                  <Route path="/locations" element={<Locations />} />
+                  <Route path="/watchlist" element={<WatchList />} />
+                </Routes>
+              </>
+            }
+          />
         </Routes>
       </div>
     </Router>
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import RickAndMorty from '/rick-and-morty.png';
+import './HomePage.css';
 
 export default function HomePage() {
+  
   return (
-    <div className="home-page">
-      
+    <div className="homePage">
+      <div className="homePageLeft">
+        <h2>Welkome to Rick and Morty</h2>
+
+
+        <p>Get to know the characters of Rick and Morty</p>
+
+
+        <button onClick={() => window.location.href = '/characters'}>Characters</button>
+
+      </div>
+
+      <div className="homePageRight"> 
+        <img src={RickAndMorty} alt="1" />
+
+      </div>
+
     </div>
-  )
+  );
 }
